86425c0ca0e765d6060d43735e21e8a76eb652d6
hexfae <hexfae@proton.me>
2024-12-18 10:30:57 +0100

    day 3

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -2,18 +2,19 @@ use std::time::Instant;
 
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
-    let input = include_str!("../inputs/day2");
-    let iterations = 100_000;
+    let input = include_str!("../inputs/day3");
+    let iterations = 10000;
     let now = Instant::now();
     for _ in 0..=iterations {
-        day2::part1::parse(input);
+        day3::part1::parse(input);
     }
     let part1_elapsed = now.elapsed().as_micros() / iterations;
     let now = Instant::now();
     for _ in 0..=iterations {
-        day2::part2::parse(input);
+        day3::part2::parse(input);
     }
     let part2_elapsed = now.elapsed().as_micros() / iterations;
     println!(
